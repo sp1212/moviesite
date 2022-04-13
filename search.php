76e5001c40d86f5517fe964cbb4955502cdef2ca
Taher Calcuttawala <tcalcuttawala@gmail.com>
2022-04-13 15:08:54 -0400

    fixed search page styling

--- search.php
+++ search.php
@@ -59,9 +59,11 @@
                                     <li class=\"list-group-item\">" . $data[$i]["genre"] . "</li>
                                     <li class=\"list-group-item\">" . $data[$i]["runtime"] . " minutes" . "</li>
                                     <li class=\"list-group-item\">" . $data[$i]["releaseDate"] . "</li>
-                                    <li class=\"list-group-item\">
+                                    <li class=\"list-group-item\"> </li>
+                                    </ul>
                         </button>
                         </form>
+                                    <li class=\"list-group-item\">
                                         <form method=\"post\">
                                             <button type=\"submit\" class=\"btn btn-warning\" name=\"favorite\" value=" . $data[$i]["imdbId"] .">Favorite</button>
                                         </form>
